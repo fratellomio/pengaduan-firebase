74e5f5625d1da80640220428f72260d9cb6342bf
fratellomio <mr.gomu@gmail.com>
2020-08-08 11:51:48 +0700

    change admin for local dev

--- functions/util/admin.js
+++ functions/util/admin.js
@@ -1,6 +1,9 @@
 const admin = require('firebase-admin');
 
-admin.initializeApp();
+var serviceAccount = require('../../pengaduan-lotim-firebase-adminsdk.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 const db = admin.firestore();
 
